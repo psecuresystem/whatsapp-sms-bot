87917599d1a0c002b0477bd134586fe8801a3511
Vision <visiondaniels32@gmail.com>
2022-09-13 11:14:24 +0000

    Fixed error

--- src/services/messageHandler.js
+++ src/services/messageHandler.js
@@ -12,10 +12,22 @@ class SmsHandler {
   async sendSms(numbers, message) {
     let numbersInString = numbers.join(',');
     let data = await axios.get(
-      `https://smsexperience.com/api/sms/sendsms?username=VisionOnyeaku&password=danz6278&sender=KWM KK&recipient=${numbersInString}&message=${message}`
+      `https://smsexperience.com/api/sms/sendsms?username=VisionOnyeaku&password=danz6278&sender=KWM%20KK&recipient=${numbersInString}&message=${replaceAll(
+        message,
+        ' ',
+        '%20'
+      )}`
     );
     return data;
   }
+}
+
+function escapeRegExp(string) {
+  return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
+}
+
+function replaceAll(str, find, replace) {
+  return str.replace(new RegExp(escapeRegExp(find), 'g'), replace);
 }
 
 module.exports = SmsHandler;
